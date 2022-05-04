48d411c6a8eb994ecd9d21cde75e710b514b156a
Sharjeel Mustafa <sharjeeliv@gmail.com>
2022-05-04 13:28:56 -0400

    Finished Project 1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn"); // We get element from HTML
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = Math.floor(getRandomNumber() * colors.length);
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.random();
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn"); // Get Elements from HTML
+const color = document.querySelector(".color");
+
+// What to do on a btn click
+btn.addEventListener("click", function () {
+  let randomHex = "#";
+  for (let i = 0; i < 6; i++) randomHex += getRandomHex();
+  console.log(randomHex);
+
+  document.body.style.backgroundColor = randomHex;
+  color.textContent = randomHex;
+});
+
+function getRandomHex() {
+  return hex[Math.floor(Math.random() * hex.length)];
+}
